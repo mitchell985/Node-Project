7bf7de8f55d8f9e85d4e3f8e21578e78466af20b
Mitchell <mcaddy0@gmail.com>
2022-07-20 05:52:36 +1200

    first commit

---
+++ src/__tests__/index.test.ts
@@ -0,0 +1,12 @@
+import supertest from "supertest";
+import { app } from "../app"
+
+describe('index', () => {
+    describe('get index route', () => {
+        describe('given the application is running correctly', () => {
+            it("should return a 200", async () => {
+                await supertest(app).get('/').expect(200)
+            })
+        })
+    })
+})
---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+export const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Nothing to see here 😄")
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`listening on port ${port}`));
