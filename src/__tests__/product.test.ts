d159f7057cb586af109bd0a77da323998d3f1cfc
Mitchell <mcaddy0@gmail.com>
2022-07-28 18:51:23 +1200

    testing git ignore

--- src/__tests__/product.test.ts
+++
@@ -1,9 +0,0 @@
-describe("product", () => {
-    describe("get product route", () => {
-        describe("given the product does not exist", () => {
-            it("should return a 404", () => {
-                expect(true).toBe(true);
-            })
-        })
-    })
-})
--- src/__tests__/user.test.ts
+++
@@ -1,9 +0,0 @@
-describe("user", () => {
-    describe("get user route", () => {
-        describe("given the user does not exist", () => {
-            it("should return a 404", () => {
-                expect(true).toBe(true);
-            })
-        })
-    })
-})
